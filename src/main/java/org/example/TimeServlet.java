5f2aa7ed7758ef38cbef5c9739f40c43e311b998
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-04-01 14:53:10 +0300

    add cookies

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -39,23 +39,36 @@ public class TimeServlet extends HttpServlet {
         resp.setContentType("text/html; charset=utf-8");
         Context context = new Context(
                 req.getLocale(),
-                Map.of("time", parseTime(req))
+                Map.of("time", parseTime(req, resp))
         );
 
         engine.process("index", context, resp.getWriter());
         resp.getWriter().close();
     }
-    private String parseTime(HttpServletRequest req) {
+    private String parseTime(HttpServletRequest req, HttpServletResponse resp) {
         DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
         ZonedDateTime zonedDateTime;
 
         if (req.getParameterMap().containsKey("timezone")) {
             String timezoneParam = req.getParameter("timezone").replace(" ", "+");
             zonedDateTime = ZonedDateTime.now(ZoneId.of(timezoneParam));
+            resp.addCookie(new Cookie("lastTimezone", timezoneParam));
         } else {
-            zonedDateTime = ZonedDateTime.now(ZoneId.of("UTC"));
+            zonedDateTime = getLastTimezone(req);
         }
 
         return zonedDateTime.format(dateFormat);
+    }
+
+    private ZonedDateTime getLastTimezone(HttpServletRequest req) {
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("lastTimezone")) {
+                    return ZonedDateTime.now(ZoneId.of(cookie.getValue()));
+                }
+            }
+        }
+        return ZonedDateTime.now(ZoneId.of("UTC"));
     }
 }
